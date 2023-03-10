411e08b79408a5087994666107d63667bdfec9d7
Replit user <>
2023-03-10 12:05:12 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -26,7 +26,7 @@ Programe la función void arrayMaxMin(int *a, int n, int *max, int *min),
 la cual encuentra el máximo y el mínimo valor del arreglo a y los
 almacena en las variables apuntadas por max y min.
 */
-int cmp (const void *pivote, cons void *element)
+int cmp (const void *pivote, const void *element)
 {
   int *ptrpivote = (int*)pivote;
   int *ptrelemen = (int*)element;
